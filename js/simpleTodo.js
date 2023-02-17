d25cad074c6cc663381e9ec4db531cf7cfeb48af
Marius CA <marius@example.com>
2023-02-17 12:36:42 +0200

    todo.html

---
+++ js/simpleTodo.js
@@ -0,0 +1,7 @@
+'use strict';
+console.log('simpleTodo.js file was loaded');
+
+// kai paspaudziam Add it mygtuka
+// paimti reiksme is input
+// sukurti auja li el su tekstu is input
+// patalpinti li sarase
