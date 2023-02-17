1c9516dc7477b73d010be9764c61f07d15c042c1
Marius CA <marius@example.com>
2023-02-17 15:47:04 +0200

    5. sukurti

--- js/uzd.js
+++ js/uzd.js
@@ -2,13 +2,21 @@
 console.log('uzd.js file was loaded');
 
 // taikomes
-
+const h1El = document.querySelector('h1');
 const listEl = document.getElementById('itemsList');
 const liElsArr = listEl.children;
-console.log('liElsArr ===', liElsArr);
 
 // 3. uzdeti visiem li elementams klase 'sigleLi' ciklo budu
 for (let oneLiEl of liElsArr) {
   oneLiEl.classList.add('sigleLi');
   // console.log('oneLiEl ===', oneLiEl);
+}
+
+// 5. sukurti mygtuka kurio paspaudimu mes uzdedam jei nea ir nuimam jei yra klase 'active' and h1 elemento
+const h1BtnEl = document.getElementById('h1Btn');
+
+h1BtnEl.addEventListener('mouseup', toggleH1Class);
+
+function toggleH1Class() {
+  h1El.classList.toggle('active');
 }
