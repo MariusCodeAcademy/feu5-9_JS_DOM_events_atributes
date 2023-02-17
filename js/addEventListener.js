8062014c5672adc3c26913c8fbfcf55ea1958fc5
Marius CA <marius@example.com>
2023-02-17 12:02:33 +0200

    inputToH1

--- js/addEventListener.js
+++ js/addEventListener.js
@@ -1,6 +1,11 @@
 'use strict';
 console.log('addEventListener.js file was loaded');
 
+// nusitaikom
+const h1El = document.querySelector('h1');
+const listEl = document.getElementById('itemsList');
+const darkModeBntEl = document.getElementById('darkModeBtn');
+
 console.log('darkModeBntEl ===', darkModeBntEl);
 // darkModeBntEl.onclick = toggleDarkMode;
 // darkModeBntEl.onclick = makeAlert;
@@ -9,8 +14,51 @@ console.log('darkModeBntEl ===', darkModeBntEl);
 // element.addEventListener('evento pavadinimas', funkcija kuri vyks ivykus eventui);
 
 darkModeBntEl.addEventListener('click', toggleDarkMode);
-darkModeBntEl.addEventListener('click', makeAlert);
+darkModeBntEl.addEventListener('click', toggleBtnText);
+// darkModeBntEl.addEventListener('mouseleave', makeAlert);
 
 function makeAlert() {
   alert('made alert');
 }
+
+// Functions
+function toggleDarkMode() {
+  listEl.classList.toggle('darkModeOn');
+}
+// <button id="darkModeBtn">Dark Mode</button>
+function toggleBtnText() {
+  // gauti texta is mygtuko
+  let currentBtnText = darkModeBntEl.textContent;
+  console.log('currentBtnText ===', currentBtnText);
+  // jei teksta prasideda su 'Dark'
+  if (currentBtnText.startsWith('Dark')) {
+    /// TAIP - pakeisti teksta i 'Light'
+    darkModeBntEl.textContent = 'Light Mode';
+  } else {
+    /// NE - pakeisti i 'Dark'
+    darkModeBntEl.textContent = 'Dark Mode';
+  }
+}
+
+// INPUT EVENTS
+
+// paimti teksta ivesta i input ir paspaudus mygtuka patalpinti ji h1 el
+const mainTitleInputEl = document.getElementById('mainTitleInput');
+const mainTitleBtnEl = document.getElementById('mainTitleBtn');
+
+mainTitleBtnEl.addEventListener('click', inputToH1);
+
+function inputToH1() {
+  console.log('inputToH1 function ran');
+  // paimti reiksme is input
+  const textInInput = mainTitleInputEl.value;
+  console.log('textInInput ===', textInInput);
+  // patalpinti text i h1
+  h1El.textContent = textInInput;
+  // isvalyti input value
+  mainTitleInputEl.value = '';
+}
+
+// <h3 id="currentValue">currentValue</h3>
+// sukurtifunkcija, kuri paima reiksme is input ir patalpina ja aukciau nurodytame elmente
+// prideti funckija prie mygtuko paspaudimo
